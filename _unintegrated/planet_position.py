e23db9b8fab26afefba58ceacbb31fa4889864cc
ion- <itornaza@gmail.com>
2024-06-06 12:19:53 +0300

    feat: Added display functions

--- _unintegrated/planet_position.py
+++ _unintegrated/planet_position.py
@@ -31,9 +31,9 @@ def subtract_angles(angle1, angle2):
     total_minutes_difference = total_minutes1 - total_minutes2
     return minutes_to_degrees_and_minutes(total_minutes_difference)
 
-def time_to_percentage(hours, minutes):
+def time_to_percentage(t):
     total_minutes_in_day = 24 * 60
-    total_minutes = hours * 60 + minutes
+    total_minutes = t[0] * 60 + t[1]
     percentage_of_day = (total_minutes / total_minutes_in_day)
     return percentage_of_day
 
@@ -44,48 +44,74 @@ def multiply_angle_by_percentage(angle, percentage):
     resulting_minutes = total_minutes * percentage
     return minutes_to_degrees_and_minutes(resulting_minutes)
 
+def get_bool(prompt):
+    user_input = input(prompt).strip().lower()
+    if user_input in ['yes', 'y', 'true', 't']:
+        return True
+    elif user_input in ['no', 'n', 'false', 'f']:
+        return False
+    else:
+        print("Invalid input. Please enter 'yes', 'no', 'true', or 'false'.")
+
+def get_hours_minutes(prompt):
+    while True:
+      try:
+          user_input = input(prompt)
+          num1, num2 = map(float, user_input.split())
+          return (num1, num2)
+      except ValueError:
+          print("Invalid input. Please enter two valid numbers separated by a space.")
+
+def get_integer(prompt):
+    while True:
+      try:
+          user_input = input(prompt)
+          integer_value = int(user_input)
+          return integer_value
+      except ValueError:
+          print("Invalid input. Please enter a valid integer.")
+
+def display_angle(a, prompt):
+    print(f"{prompt}:\t {a[0]:.0f}° {a[1]:.0f}\'")
+
+def display_percentage(f):
+    print(f"Day percentage:\t\t {f:.4f}")
+
 ###############################################################################
 #                                   USER INPUT                                #
 ###############################################################################
 
-# Is the planet in retrograde motion?
-rx = False
+# Get the time we want to calculate the angle for
+t = get_hours_minutes("Time? ")
+
+# Is the planet in retrograde motion? 
+rx = get_bool("Retrograde? ")
 
 # Get the angles from ephimeris in the order they appear there
-prev_angle = (1, 58)
-next_angle = (3, 11)  
-
-# Get the time we want to calculate the angle for
-hours = 2
-minutes = 52
+a = get_hours_minutes("Set the first angle in degrees and minutes: ")
+b = get_hours_minutes("Set the second angle in degrees and minutes: ")
 
 ###############################################################################
 #                                  CALCULATIONS                               #
 ###############################################################################
 
-# 1. Calculate the difference between the angles
-if rx:
-  delta_angle = subtract_angles(prev_angle, next_angle)
-else:
-  delta_angle = subtract_angles(next_angle, prev_angle)
+# Calculate the difference between the angles
+d = subtract_angles(a, b) if rx else subtract_angles(b, a)
 
 # Get the percentage of the day as a factor
-percentage = time_to_percentage(hours, minutes)
+f = time_to_percentage(t)
 
-# 3. Calculate the angle difference from that time to the previous angle
-resulting_angle = multiply_angle_by_percentage(delta_angle, percentage)
+# Calculate the angle difference from that time to the previous angle
+theta = multiply_angle_by_percentage(d, f)
 
-# 4. Calculate the resulting absolute angle
-if rx:
-  result = subtract_angles(prev_angle, resulting_angle)
-else: 
-  result = add_angles(prev_angle, resulting_angle)
+# Calculate the resulting absolute angle
+phi = subtract_angles(a, theta) if rx else add_angles(a, theta)
 
 ###############################################################################
 #                                     REPORT                                  #
 ###############################################################################
 
-print("Angles difference:\t", delta_angle)
-print(f"Day percentage ({hours}, {minutes}):\t {percentage:.4f}")
-print(f"Angle diff at time:\t ({resulting_angle[0]}, {resulting_angle[1]:.4f})")
-print(f"Position:\t\t ({result[0]:.0f}, {result[1]:.0f})")
+display_angle(d, "Angles difference")
+display_percentage(f)
+display_angle(theta, "Angle diff at time")
+display_angle(phi, "Position at time")
