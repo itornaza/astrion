6e9a9ac44d1475c9efa31f0aea737600f67858a5
ion- <itornaza@gmail.com>
2024-10-31 16:37:06 +0200

    feat: Added a file for eclipses

---
+++ eclipses.py
@@ -0,0 +1,30 @@
+#
+# eclipses
+#
+
+class Eclipse:
+    # TODO
+    pass
+
+class Eclipses:
+    # TODO: Keywords and characteristics
+    # Solar, Lunar
+    # Saros cycle
+    # Sign, House, Planetary ruler (conjuncting planet within 5 orb), 
+    # Umbra over the location, Partia/Annular/Total
+    # Import/Export axis, 
+    pass
+
+class Saros:
+    # TODO
+    pass
+
+class Saroses:
+    # TODO: Keywords and characteristics
+    # Sign, 1st eclipse in the cycle, current phase of the cycle
+    # North/South node series
+    pass
+
+if __name__ == "__main__":
+    # TODO
+    pass    
