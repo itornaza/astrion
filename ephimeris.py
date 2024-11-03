de3ad21cab982d085664957a1ddab7079de698a1
ion- <itornaza@gmail.com>
2024-11-03 20:06:43 +0200

    feat: Working solution for the ecliptic long of all planets plus Chiron

--- ephimeris.py
+++ ephimeris.py
@@ -9,44 +9,47 @@ from skyfield.framelib import ecliptic_frame # type: ignore
 from constants import *
 
 class Ephimeris():
-    def __init__(self, utc_time):
+
+    # TODO: Get time up to the minute
+
+    def __init__(self, year: int, month: int, day: int, hour: int):
         """Get all planet longitudes for a given UTC time. Note that Chiron
         is not supported in skyfield. The provided longitude is given by
         the library directly in tropical zodiac"""
 
         # Load ephimeris
         self.ephimeris_ = load('de440.bsp') 
+        ts = load.timescale()
+        utc_time = ts.utc(year, month, day, hour)
         self.earth_ = self.ephimeris_[EARTH].at(utc_time)
 
-        # Load solar system data
-        self.sun_ = self.earth_.observe(self.ephimeris_[SUN]).apparent()
-        self.moon_ = self.earth_.observe(self.ephimeris_[MOON]).apparent()
-        self.mercury_ = self.earth_.observe(self.ephimeris_[MERCURY]).apparent()
-        self.venus_ = self.earth_.observe(self.ephimeris_[VENUS]).apparent()
-        self.mars_ = self.earth_.observe(self.ephimeris_['MARS BARYCENTER']).apparent()
-        self.jupiter_ = self.earth_.observe(self.ephimeris_['JUPITER BARYCENTER']).apparent()
-        self.saturn_ = self.earth_.observe(self.ephimeris_['SATURN BARYCENTER']).apparent()
-        self.uranus_ = self.earth_.observe(self.ephimeris_['URANUS BARYCENTER']).apparent()
-        self.neptune_ = self.earth_.observe(self.ephimeris_['NEPTUNE BARYCENTER']).apparent()
-        self.pluto_ = self.earth_.observe(self.ephimeris_['PLUTO BARYCENTER']).apparent()
-        # Chiron unsupported
+        # Get the longitude of all planets except unsupported Chiron
+        _, self.sun_, _ = self.earth_.observe(self.ephimeris_[SUN]).apparent().frame_latlon(ecliptic_frame)
+        _, self.moon_, _ = self.earth_.observe(self.ephimeris_[MOON]).apparent().frame_latlon(ecliptic_frame)
+        _, self.mercury_, _ = self.earth_.observe(self.ephimeris_[MERCURY]).apparent().frame_latlon(ecliptic_frame)
+        _, self.venus_, _ = self.earth_.observe(self.ephimeris_[VENUS]).apparent().frame_latlon(ecliptic_frame)
+        _, self.mars_, _ = self.earth_.observe(self.ephimeris_['MARS BARYCENTER']).apparent().frame_latlon(ecliptic_frame)
+        _, self.jupiter_, _ = self.earth_.observe(self.ephimeris_['JUPITER BARYCENTER']).apparent().frame_latlon(ecliptic_frame)
+        _, self.saturn_, _ = self.earth_.observe(self.ephimeris_['SATURN BARYCENTER']).apparent().frame_latlon(ecliptic_frame)
+        _, self.uranus_, _ = self.earth_.observe(self.ephimeris_['URANUS BARYCENTER']).apparent().frame_latlon(ecliptic_frame)
+        _, self.neptune_, _ = self.earth_.observe(self.ephimeris_['NEPTUNE BARYCENTER']).apparent().frame_latlon(ecliptic_frame)
+        _, self.pluto_, _ = self.earth_.observe(self.ephimeris_['PLUTO BARYCENTER']).apparent().frame_latlon(ecliptic_frame)
 
-        planets = [self.sun_, self.moon_, self.mercury_, self.venus_, self.mars_,
+        self.planets_long = [self.sun_, self.moon_, self.mercury_, self.venus_, self.mars_,
                    self.jupiter_, self.saturn_, self.uranus_, self.neptune_,
                    self.pluto_]
         
-        planet_names = [SUN, MOON, MERCURY, VENUS, MARS, JUPITER, SATURN, URANUS, NEPTUNE,
-                 PLUTO]
-        
+        self.planet_names = [SUN, MOON, MERCURY, VENUS, MARS, JUPITER, SATURN, URANUS, NEPTUNE,
+                        PLUTO]
+
+    def print_all(self):
         print("\n* EPHIMERIS - LONGITUDES *\n")
         for i in range(10):
-            _, long, _ = planets[i].frame_latlon(ecliptic_frame)
-            self.print_planet_position(planet_names[i], long)
+            self.print(self.planet_names[i], self.planets_long[i])
 
-    def print_planet_position(self, planet, long):
+    def print(self, planet, long):
         print(planet + ": {:.5f}°".format(long.degrees))
 
 if __name__ == "__main__":
-    ts = load.timescale()
-    eph = Ephimeris(ts.utc(2024, 11, 2, 21))
-    eph.get_aries_zero()
+    eph = Ephimeris(2024, 11, 2, 21)
+    eph.print_all()
--- horizons.py
+++ horizons.py
@@ -12,71 +12,65 @@ from constants import *
 
 class HorizonsError(Exception):
     """Exception raised when the NASA API response does not contain floats"""
-    def __init__(self, message="There are no floating point numbers in the response"):
+    def __init__(self, message="No floating point numbers in response"):
         self.message = message
         super().__init__(self.message)
 
-class planet_id():
-    chiron: str = "2060"
+class Horizons():
 
-def extract_longitude(text):
-    placeholder = r'\$\$SOE(.*?)\$\$EOE'
-    response = re.findall(placeholder, text, re.DOTALL)[0]
-    is_float = r'[-+]?\d*\.\d+'
-    coordinates = re.findall(is_float, response)
-    if not coordinates:
-        raise HorizonsError(response)
-    return float(coordinates[0])
+    def __init__(self, year, month, day, hour, min):
+        """Get the longitude of Chiron on the ecliptic. We get the positions
+        of all other planets from the ephimeris module"""
+        
+        self.chiron_: float = 0.0
 
-def get_planet_long(planet_id: int, planet_name: str):
-    # Define today's date and set time to 12:00 UTC
-    now_utc = datetime.now(timezone.utc)
-    today_date = now_utc.date()
-    start_time = datetime(today_date.year, today_date.month, today_date.day, 21, 0, 0, tzinfo=timezone.utc)
-    stop_time = start_time + timedelta(minutes=1)
+        t1_prep = datetime(year, month, day, hour, min, 0, tzinfo=timezone.utc)
+        t2_prep = t1_prep + timedelta(minutes=1)
+        t1 = t1_prep.strftime('%Y-%m-%dT%H:%M')
+        t2 = t2_prep.strftime('%Y-%m-%dT%H:%M')
 
-    # Format dates for Horizons (ISO 8601 format with 'T' between date and time)
-    f_start_time = start_time.strftime('%Y-%m-%dT%H:%M')
-    f_stop_time = stop_time.strftime('%Y-%m-%dT%H:%M')
+        chiron_id: str = "2060"
+        params = {
+            "format": "json",
+            "COMMAND": chiron_id,
+            "EPHEM_TYPE": "OBSERVER",
+            "REF_PLANE": "ECLIPTIC",
+            "CENTER": "500@399",
+            "START_TIME": t1,
+            "STOP_TIME": t2,
+            "QUANTITIES": "18",  # Ecliptic coordinates
+            "ANG_FORMAT": "DEG"  
+        }
 
-    # Parameters for the given planet in ecliptic coordinates) with the center 
-    # set to Earth
-    params = {
-        "format": "json",
-        "COMMAND": planet_id,
-        "EPHEM_TYPE": "OBSERVER",
-        "REF_PLANE": "ECLIPTIC",
-        "CENTER": "500@399",
-        "START_TIME": f_start_time,
-        "STOP_TIME": f_stop_time,
-        "QUANTITIES": "18",  
-        "ANG_FORMAT": "DEG"  
-    }
+        horizons_url = "https://ssd.jpl.nasa.gov/api/horizons.api"
+        request_url = f"{horizons_url}?{urllib.parse.urlencode(params)}"
+        try:
+            with urllib.request.urlopen(request_url) as response:
+                position = response.read()
+                self.chiron_ = self.extract_long(position.decode())
+                self.print()
+        except HorizonsError as e:
+            print(f"NASA error occured: {e.message}")
+        except HTTPError as e:
+            print(f"HTTP error occurred: {e.code} - {e.reason}")
+        except URLError as e:
+            print(f"URL error occurred: {e.reason}")
+        except TypeError as e:
+            print(f"Type error occured: {e}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
-    # NASA JPL Horizons API URL
-    horizons_url = "https://ssd.jpl.nasa.gov/api/horizons.api"
+    def print(self):
+        print(CHIRON + ":", self.chiron_)
 
-    # Prepare request
-    request_url = f"{horizons_url}?{urllib.parse.urlencode(params)}"
-    print(planet_name + ":", end=" ")
-
-    # Send request
-    try:
-        with urllib.request.urlopen(request_url) as response:
-            position = response.read()
-            print(extract_longitude(position.decode()))
-
-    except HorizonsError as e:
-        print(f"NASA error occured: {e.message}")
-        # TODO - for debugging: print(position.decode())
-    except HTTPError as e:
-        print(f"HTTP error occurred: {e.code} - {e.reason}")
-    except URLError as e:
-        print(f"URL error occurred: {e.reason}")
-    except TypeError as e:
-        print(f"Type error occured: {e}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    def extract_long(self, text):
+        placeholder = r'\$\$SOE(.*?)\$\$EOE'
+        response = re.findall(placeholder, text, re.DOTALL)[0]
+        is_float = r'[-+]?\d*\.\d+'
+        coordinates = re.findall(is_float, response)
+        if not coordinates:
+            raise HorizonsError(response)
+        return float(coordinates[0])
 
 if __name__ == "__main__":
-    get_planet_long(planet_id.chiron, CHIRON)
+    chiron = Horizons(2024, 11, 2, 21, 0)
